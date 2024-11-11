0e3910ea6d8faf3970c236118b8b17d1894668bc
jinwp <jinwoong0925@kaist.ac.kr>
2024-11-11 19:48:31 +0900

    dotenv settings

---
+++ src/postings/dto/create-posting.dto.ts
@@ -0,0 +1,15 @@
+import { IsOptional, IsString, MaxLength } from "class-validator";
+
+export class CreatePostingDto {
+  @IsString({
+    message: '제목은 문자열로만 가능합니다',
+  })
+  @MaxLength(255, {
+    message: '내용이 너무 깁니다',
+  })
+  text: string;
+
+  @IsOptional()
+  @IsString()
+  semester: string;
+}
